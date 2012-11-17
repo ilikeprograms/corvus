96d505494c7b0aa54971e3ef4e62bd4f257ef4cf
Thomas Coleman <tom@ilikeprograms.com>
2012-11-17 13:26:20 +0000

    FileLocator now finds FrontendBundle routing.yml. Navigation can be edited again

--- src/Corvus/AdminBundle/Form/Type/NavigationType.php
+++ src/Corvus/AdminBundle/Form/Type/NavigationType.php
@@ -52,16 +52,30 @@ class NavigationType extends AbstractType
 
 	private function getRouteChoices()
 	{
-		$locator = new FileLocator(__DIR__.'/../../../FrontendBundle/resources/config');
+		// Find the current directory and split it into an array minus the /
+		$pathArray = explode("/", __DIR__);
+		// Unset the last 3 array items
+		for ($i = 0; $i < 3; $i++)
+		{
+			unset($pathArray[count($pathArray) - 1]);
+		}
+		// Implode the array, new __DIR__ should be src/Corvus
+		$path = implode("/", $pathArray);
+
+		// Set the FileLocator directory to be src/Corvus/FrontendBundle/resources/config
+		$locator = new FileLocator($path."/FrontendBundle/resources/config");
 		$loader = new YamlFileLoader($locator);
+		// Load the routing file
 		$collection = $loader->load('routing.yml');
 
 		$routeNames = array();
 
+		// Retrieve the route information and put it in the array
 		foreach ($collection as $key => $value) {
 		    $routeNames[$value->getPattern()] = $value->getPattern();
 		}
 
+		// Return the array of route names
 		return $routeNames;
 	}
 }
