b71c2af59888b68a1c13e87001e982031a84b822
Thomas Coleman <tom@ilikeprograms.com>
2013-04-21 17:37:22 +0100

    Made all of the templates load from the appropriate folder based on the template_choice

--- src/Corvus/FrontendBundle/Controller/DefaultController.php
+++ src/Corvus/FrontendBundle/Controller/DefaultController.php
@@ -7,39 +7,44 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 use Corvus\AdminBundle\Entity\Contact;
 use Corvus\AdminBundle\Form\Type\ContactType;
+use Corvus\FrontendBundle\Extension\PortfolioInfoRepository as PortfolioInfo;
 
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CorvusFrontendBundle:Default:home.html.twig');
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
+        return $this->render('CorvusFrontendBundle:'.$template_choice.':home.html.twig');
     }
 
     public function educationAction()
     {
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
         $education = $this->getDoctrine()
             ->getRepository('CorvusAdminBundle:Education')
             ->findBy(array(), array('start_date' => 'DESC'));
 
-        return $this->render('CorvusFrontendBundle:Default:education.html.twig', array(
+        return $this->render('CorvusFrontendBundle:'.$template_choice.':education.html.twig', array(
             'education' => $education,
         ));
     }
 
     public function skillsAction()
     {
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
         $skills = $this->getDoctrine()
             ->getRepository('CorvusAdminBundle:Skills')
             ->FindAll();
 
-        return $this->render('CorvusFrontendBundle:Default:skills.html.twig', array(
+        return $this->render('CorvusFrontendBundle:'.$template_choice.':skills.html.twig', array(
             'skills' => $skills,
         ));
     }
 
     public function aboutAction(Request $request)
     {
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
     	$em = $this->getDoctrine()->getEntityManager();
     	$about = $em->getRepository('CorvusAdminBundle:About')->Find(1);
 
@@ -60,14 +65,14 @@ class DefaultController extends Controller
                 ->setSubject('Email from iLikePrograms.com')
                 ->setFrom('tomc2k3@googlemail.com')
                 ->setTo('tomc_2k3@hotmail.com')
-                ->setBody($this->renderView('CorvusFrontendBundle:Default:contact.txt.twig', array('contact' => $data)));
+                ->setBody($this->renderView('CorvusFrontendBundle:'.$template_choice.':contact.txt.twig', array('contact' => $data)));
 
             $this->get('mailer')->send($message);
 
             return $this->redirect($this->generateUrl('CorvusFrontendBundle_About'));
         }
 
-    	return $this->render('CorvusFrontendBundle:Default:about.html.twig', array(
+    	return $this->render('CorvusFrontendBundle:'.$template_choice.':about.html.twig', array(
     		'about' => $about,
             'form' => $form->createView()
     	));
@@ -75,20 +80,22 @@ class DefaultController extends Controller
 
     public function workHistoryAction()
     {
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
         $workHistory = $this->getDoctrine()
             ->getRepository('CorvusAdminBundle:WorkHistory')->FindAll();
 
-        return $this->render('CorvusFrontendBundle:Default:workHistory.html.twig', array(
+        return $this->render('CorvusFrontendBundle:'.$template_choice.':workHistory.html.twig', array(
             'workHistory' => $workHistory
         ));
     }
     
     public function projectHistoryAction()
     {
+        $template_choice = $this->container->get('portfolioinforepository')->getTemplateChoice();
         $projectHistory = $this->getDoctrine()
             ->getRepository('CorvusAdminBundle:ProjectHistory')->FindAll();
 
-        return $this->render('CorvusFrontendBundle:Default:projectHistory.html.twig', array(
+        return $this->render('CorvusFrontendBundle:'.$template_choice.':projectHistory.html.twig', array(
             'projectHistory' => $projectHistory
         ));
     }
