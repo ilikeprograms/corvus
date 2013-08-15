7c341f6558a7c4ec3bec429fa64a067d15a1493b
Thomas Coleman <tom@ilikeprograms.com>
2013-08-15 16:29:58 +0100

    Controllers which relate to TableViews no inherit from the TableViewContoller and as such inherit the common actions
    Variables to use in the parent TableViewController are instantiated and
    sent to the parent using the constructor. The parent can then use that
    to perform the functionality in the xAction's it provides.

--- src/Corvus/AdminBundle/Controller/ProjectHistoryController.php
+++ src/Corvus/AdminBundle/Controller/ProjectHistoryController.php
@@ -2,129 +2,22 @@
 
 namespace Corvus\AdminBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-use Corvus\AdminBundle\Entity\ProjectHistory;
-use Corvus\AdminBundle\Form\Type\ProjectHistoryType;
+use Corvus\AdminBundle\Entity\ProjectHistory,
+    Corvus\AdminBundle\Form\Type\ProjectHistoryType,
+    Corvus\AdminBundle\Entity\ProjectHistoryTableView,
+    Corvus\AdminBundle\ILP\Controller\TableViewController;
 
 
-class ProjectHistoryController extends Controller
+class ProjectHistoryController extends TableViewController
 {
-    public function newAction(Request $request)
+    public function __construct()
     {
-        $projectHistory = new ProjectHistory();
-
-        $form = $this->createForm(new ProjectHistoryType(), $projectHistory);
-        $form->remove('check');
-
-        if ($request->getMethod() == 'POST') {
-            $form->bind($request);
-
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getEntityManager();
-                //$projectHistory->images = $form->get('images')->getData();
-                foreach ($projectHistory->images as $key => $value) {
-                    $value->ProjectHistory = $projectHistory;
-                }
-                $em->persist($projectHistory);
-                $em->flush();
-
-                $this->get('session')->setFlash('notice', 'New Project History was added!');
-
-                return $this->redirect($this->generateUrl('CorvusAdminBundle_ProjectHistory'));
-            } else {
-                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
-            }
-        }
-
-        return $this->render('CorvusAdminBundle:Default:newProjectHistory.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function editAction($id, Request $request)
-    {
-        $projectHistory = $this->getDoctrine()
-            ->getRepository('CorvusAdminBundle:ProjectHistory')
-            ->find($id);
-
-        if (!$projectHistory) {
-            throw $this->createNotFoundException('No Project History found with id '.$id);
-        }
-
-        $thumbnails = array();
-
-        if($projectHistory->images) {
-            foreach ($projectHistory->getImages()->getValues() as $key ) {
-                $path = $key->getPath();
-                $extension = substr($path, strrpos($path, '.' ));
-                $thumbnails[] = str_replace($extension, "", $path)."_thumb".$extension;
-            }
-        }
-
-        $form = $this->createForm(new ProjectHistoryType(), $projectHistory);
-        $form->remove('check');
-
-        if ($request->getMethod() == 'POST') {
-            $form->bind($request);
-
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getEntityManager();
-                $projectHistory->updated = new \DateTime("now");
-
-                foreach ($projectHistory->images as $key => $value) {
-                    $value->ProjectHistory = $projectHistory;
-                }
-
-                $em->persist($projectHistory);
-                $em->flush();
-
-                $this->get('session')->setFlash('notice', 'Your changes were saved!');
-                
-                return $this->redirect($this->generateUrl('CorvusAdminBundle_ProjectHistory'));
-            } else {
-                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
-            }
-        }
-
-        return $this->render('CorvusAdminBundle:Default:editProjectHistory.html.twig', array(
-            'form' => $form->createView(), 'thumbnails' => $thumbnails,
-        ));
-    }
-
-    public function deleteAction($id, Request $request)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-        $data = $request->request->get('projectHistoryTableView');
-
-        if(isset($data)) {
-            foreach ($data['projectHistory'] as $key => $value) {
-                if(is_int((int)$value['check']) == true)
-                {
-                    $phToRemove = $em
-                       ->getRepository('CorvusAdminBundle:ProjectHistory')
-                       ->Find($value['check']);
-
-                    if(isset($phToRemove))
-                    {
-                        $em->remove($phToRemove);
-                    }
-                }
-            }
-        }
-        if(!$id == null)
-        {
-            $phToRemove = $em->getRepository('CorvusAdminBundle:ProjectHistory')
-                ->Find($id);
-
-            $em->remove($phToRemove);
-        }
-
-        $em->flush();
-
-        $this->get('session')->setFlash('notice', 'Selected Project History was deleted!');
-        return $this->redirect($this->generateUrl('CorvusAdminBundle_ProjectHistory'));
+        // Give the TableViewController parent the data it needs to construct the table view and methods
+        parent::__construct(
+            new ProjectHistory(),
+            new ProjectHistoryType(),
+            ProjectHistoryTableView::getDataName(),
+            ProjectHistoryTableView::getTypeName()
+        );
     }
 }
--- src/Corvus/AdminBundle/Controller/WorkHistoryController.php
+++ src/Corvus/AdminBundle/Controller/WorkHistoryController.php
@@ -2,109 +2,22 @@
 
 namespace Corvus\AdminBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-use Corvus\AdminBundle\Entity\WorkHistory;
-use Corvus\AdminBundle\Form\Type\WorkHistoryType;
+use Corvus\AdminBundle\Entity\WorkHistory,
+    Corvus\AdminBundle\Form\Type\WorkHistoryType,
+    Corvus\AdminBundle\Entity\WorkHistoryTableView,
+    Corvus\AdminBundle\ILP\Controller\TableViewController;
 
 
-class WorkHistoryController extends Controller
+class WorkHistoryController extends TableViewController
 {
-    public function newAction(Request $request)
+    public function __construct()
     {
-        $workHistory = new WorkHistory();
-
-        $form = $this->createForm(new WorkHistoryType(), $workHistory);
-        $form->remove('check');
-
-        if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
-
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($workHistory);
-                $em->flush();
-
-                $this->get('session')->setFlash('notice', 'New Work History was added!');
-
-                return $this->redirect($this->generateUrl('CorvusAdminBundle_WorkHistory'));
-            } else {
-                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
-            }
-        }
-
-        return $this->render('CorvusAdminBundle:Default:newWorkHistory.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function editAction($id, Request $request)
-    {
-        $workHistory = $this->getDoctrine()
-            ->getRepository('CorvusAdminBundle:WorkHistory')
-            ->find($id);
-
-        if (!$workHistory) {
-            throw $this->createNotFoundException('No Work History found with id '.$id);
-        }
-
-        $form = $this->createForm(new WorkHistoryType(), $workHistory);
-        $form->remove('check');
-
-        if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
-
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($workHistory);
-                $em->flush();
-
-                $this->get('session')->setFlash('notice', 'Your changes were saved!');
-                return $this->redirect($this->generateUrl('CorvusAdminBundle_WorkHistory'));
-            } else {
-                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
-            }
-        }
-
-        return $this->render('CorvusAdminBundle:Default:editWorkHistory.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function deleteAction($id, Request $request)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $data = $request->request->get('workHistoryTableView');
-
-        if(isset($data)) {
-            foreach ($data['workHistory'] as $key => $value) {
-                if(is_int((int)$value['check']) == true)
-                {
-                    $whToRemove = $em
-                       ->getRepository('CorvusAdminBundle:WorkHistory')
-                       ->Find($value['check']);
-
-                    if(isset($whToRemove))
-                    {
-                        $em->remove($whToRemove);
-                    }
-                }
-            }
-        }
-        if(!$id == null)
-        {
-            $whToRemove = $em->getRepository('CorvusAdminBundle:WorkHistory')
-                ->Find($id);
-
-            $em->remove($whToRemove);
-        }
-
-        $em->flush();
-
-        $this->get('session')->setFlash('notice', 'Selected Work History was deleted!');
-        return $this->redirect($this->generateUrl('CorvusAdminBundle_WorkHistory'));
+        // Give the TableViewController parent the data it needs to construct the table view and methods
+        parent::__construct(
+            new WorkHistory(),
+            new WorkHistoryType(),
+            WorkHistoryTableView::getDataName(),
+            WorkHistoryTableView::getTypeName()
+        );
     }
 }
