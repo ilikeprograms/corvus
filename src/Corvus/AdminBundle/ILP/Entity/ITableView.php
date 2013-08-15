7d34eb13c54ba008a77973d38af0dd759e06064b
Thomas Coleman <tom@ilikeprograms.com>
2013-08-15 15:46:57 +0100

    New Interfaces that xEntity and xTableView should implement to enforce conformance with the TableViewController

---
+++ src/Corvus/AdminBundle/ILP/Entity/ITableView.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Corvus\AdminBundle\ILP\Entity;
+
+interface ITableView
+{
+    public static function getDataName();
+    public static function getTypeName();
+}
---
+++ src/Corvus/AdminBundle/ILP/Entity/ITableViewEntity.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Corvus\AdminBundle\ILP\Entity;
+
+interface ITableViewEntity
+{
+    public static function getName();
+    public static function getRepoName();
+}
