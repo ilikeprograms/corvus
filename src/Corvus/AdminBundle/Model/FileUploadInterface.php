823270aa8b13196873c83cf26e5b41c75bb4c934
Thomas Coleman <tom@ilikeprograms.com>
2013-09-01 16:50:18 +0100

    Removed the getAbsolutePath and getWebPath methods which are no longer useful

--- src/Corvus/AdminBundle/Model/FileUploadInterface.php
+++ src/Corvus/AdminBundle/Model/FileUploadInterface.php
@@ -3,14 +3,13 @@
 // src/Corvus/AdminBundle/Model/FileUploadInterface.php
 namespace Corvus\AdminBundle\Model;
 
-
+/**
+ * Interface which defines two functions to help with finding the Upload path,
+ * to be used in Conjunction with the FileUpload Entity to enforce conformance
+ * with the way the FileUploader service works.
+ */
 interface FileUploadInterface
 {
-	public function getAbsolutePath();
-
-    public function getWebPath();
-
     public function getUploadRootDir();
-
     public function getUploadDir();
 }
