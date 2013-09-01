cf8b58f13d455cd944977a2123745cdcd4db42e5
Thomas Coleman <tom@ilikeprograms.com>
2013-09-01 18:14:08 +0100

    Changed the FileUpload Entity to enable it be the base class which enables an Entity to upload files. Implements FileUploadInterface, FileAccessorInterface.

--- src/Corvus/AdminBundle/Entity/FileUpload.php
+++ src/Corvus/AdminBundle/Entity/FileUpload.php
@@ -3,28 +3,75 @@
 // src/Corvus/AdminBundle/Entity/FileUpload.php
 namespace Corvus\AdminBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection,
+    Corvus\AdminBundle\Model\FileUploadInterface,
+    Corvus\AdminBundle\Model\FileAccessorInterface;
 
-class FileUpload
+class FileUpload implements FileUploadInterface, FileAccessorInterface
 {
-    public function getAbsolutePath()
+    /**
+     * @var ArrayCollection $files
+     */
+    protected $files;
+    
+    public function __construct()
     {
-        return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+        $this->files = new ArrayCollection();
     }
-
-    public function getWebPath()
-    {
-        return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
-    }
-
+    
+    /**
+     * Get the Directory location of the Default Uploads Directory
+     */
     public function getUploadRootDir()
     {
-        // the absolute directory path where uploaded documents should be saved
-        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+        // the absolute directory path where uploaded files should be saved
+        return __DIR__.'/../../../../web/' . FileUpload::getUploadDir();
     }
 
+    /**
+     * Get the Default Upload directory name.
+     */
     public function getUploadDir()
     {
         // get rid of the __DIR__ so it doesn't screw when displaying uploaded doc/image in the view.
         return 'uploads';
+    }
+    
+    /**
+     * Get files
+     * 
+     * @return ArrayCollection
+     */
+    public function getFiles()
+    {
+        return $this->files;
+    }
+    
+    /**
+     * Add file
+     *
+     * @param Corvus\AdminBundle\Entity\File $file
+     */
+    public function addFile(\Corvus\AdminBundle\Entity\File $file)
+    {
+        $this->files[] = $file;
+    }
+    
+    /**
+     * Remove file
+     *
+     * @param Corvus\AdminBundle\Entity\File $file
+     */
+    public function removeFile(\Corvus\AdminBundle\Entity\File $file)
+    {
+        $this->files->remove($file);
+    }
+    
+    /**
+     * Clears the Files ArrayCollection
+     */
+    public function removeAllFiles()
+    {
+        $this->files->clear();
     }
 }
