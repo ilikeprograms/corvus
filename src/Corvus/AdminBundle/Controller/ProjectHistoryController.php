f481d2a19477046c30e874fd8284cb47e125d7a9
Thomas Coleman <tom@ilikeprograms.com>
2013-09-01 18:50:10 +0100

    Made the ProjectHistoryController new and edit actions enable support for File uploading
    Inside the actions once the form is validated, each File is persisted.
    Then the File Uploader service handles the File Entity preparation
    before persiting, and handles the uploading of the actual file itself,
    aswell as creating thumbnail images if applicable.
    
    In the edit action the findEntityFiles method created earlier is used
    to find all Files belonging to the Entity. Thumbnail are then added to
    the $thumbnails array, which are either a thumbnail image if they
    exist, or the full image if there is no thumbnail. The Files Entities
    are then added to the ProjectHistory Entity. This enables the Files to
    be managed and thumbnails displayed next to the Files in the view.

--- src/Corvus/AdminBundle/Controller/ProjectHistoryController.php
+++ src/Corvus/AdminBundle/Controller/ProjectHistoryController.php
@@ -3,7 +3,9 @@
 // src/Corvus/AdminBundle/Controller/ProjectHistoryController.php
 namespace Corvus\AdminBundle\Controller;
 
-use Corvus\AdminBundle\Entity\ProjectHistory,
+use Symfony\Component\HttpFoundation\Request,
+
+    Corvus\AdminBundle\Entity\ProjectHistory,
     Corvus\AdminBundle\Form\Type\ProjectHistoryType,
     Corvus\AdminBundle\Entity\ProjectHistoryTableView,
     Corvus\AdminBundle\ILP\Controller\TableViewController;
@@ -20,5 +22,103 @@ class ProjectHistoryController extends TableViewController
             ProjectHistoryTableView::getDataName(),
             ProjectHistoryTableView::getTypeName()
         );
+    }
+    
+    public function newAction(Request $request)
+    {        
+        $form = $this->createForm($this->_formType, $this->_entity);
+        $form->remove('check');
+
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getEntityManager();
+                
+                foreach ($this->_entity->getFiles() as $files)
+                {
+                    $em->persist($files);
+                }
+                
+                $em->persist($this->_entity);
+                $em->flush();
+
+                $this->get('session')->setFlash('notice', 'New Project History was added!');
+                return $this->redirect($this->generateUrl('CorvusAdminBundle_' . $this->_entity->getRepoName()));
+            } else {
+                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
+            }
+        }
+
+        return $this->render('CorvusAdminBundle:Default:new' . $this->_entity->getRepoName() . '.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    
+    public function editAction($id, Request $request)
+    {
+        $this->_entity = $this->getDoctrine()
+            ->getRepository('CorvusAdminBundle:' . $this->_entity->getRepoName())
+            ->find($id);
+
+        if (!$this->_entity) {
+            throw $this->createNotFoundException('No ' . $this->getRepoName() . ' found with id ' . $id);
+        }
+        
+        $files = $this->getDoctrine()
+            ->getRepository('CorvusAdminBundle:' . $this->_entity->getRepoName())
+            ->findEntityFiles($id);
+
+        $thumbnails = array();
+
+        if ($files) {
+            foreach ($files as $file) {
+                if ($file->getFileType() === 'image') {
+                    $filename = $file->getFilename();
+                    $extension = substr($filename, strrpos($filename, '.' ));
+                    $thumbnailFileName = str_replace($extension, "", $filename)."_thumb".$extension;
+                    
+                    if (file_exists($this->_entity->getUploadRootDir() . '/' .$thumbnailFileName)) {
+                        $thumbnails[$filename] = $thumbnailFileName;
+                    } else {
+                        $thumbnails[$filename] = $filename;
+                    }
+                }
+                
+                $this->_entity->addFile($file);
+            }
+        }
+
+        $form = $this->createForm($this->_formType, $this->_entity);
+        $form->remove('check');
+        $form->remove('row_order');
+        
+
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+                
+            if ($form->isValid() || !$form->getErrors()) {
+                $em = $this->getDoctrine()->getEntityManager();
+                
+                $this->_entity->setUpdated(new \DateTime("now"));
+                
+                foreach ($this->_entity->getFiles() as $file) {
+                    $em->persist($file);
+                }
+                
+                $em->persist($this->_entity);
+                $em->flush();
+
+                $this->get('session')->setFlash('notice', 'Your changes were saved!');
+
+               return $this->redirect($this->generateUrl('CorvusAdminBundle_' . $this->_entity->getRepoName()));
+            } else {
+                $this->get('session')->setFlash('notice', 'Please correct the errors to continue!');
+            }
+        }
+        
+        return $this->render('CorvusAdminBundle:Default:edit' . $this->_entity->getRepoName() . '.html.twig', array(
+            'form' => $form->createView(), 'thumbnails' => $thumbnails,
+        ));
     }
 }
