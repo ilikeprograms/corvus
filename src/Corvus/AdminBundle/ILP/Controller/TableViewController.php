a69dace0e733e3f0edb340d00caebc44764bfe6b
Thomas Coleman <tom@ilikeprograms.com>
2013-09-14 10:02:48 +0100

    In the New action, made the row_order field be set automatically.
    Made the New action use the BaseEntityRepository findMaxRowOrder method
    to find the MAX(row_order). This is then incremented and the row_order
    is set for the Entity automatically.

--- src/Corvus/AdminBundle/ILP/Controller/TableViewController.php
+++ src/Corvus/AdminBundle/ILP/Controller/TableViewController.php
@@ -27,8 +27,18 @@ abstract class TableViewController extends AbstractTableViewController
         $form = $this->createForm($this->_formType, $this->_entity);
         $form->remove('check');
 
-        if ($request->getMethod() == 'POST') {
-            $form->bind($request);
+        if ($request->getMethod() == 'POST') {            
+            // Find the Max row order for this Entity
+            $maxRowOrder = $this->getDoctrine()
+                ->getRepository('CorvusAdminBundle:' . $this->_entity->getRepoName())
+                ->findMaxRowOrder();
+            
+            // Increase the row_order by 1
+            $this->_entity->setRowOrder($maxRowOrder + 1);
+            /* Rebind the Request, the row order is now set and validated
+             * without the user needing to set it
+             */
+            $form->bindRequest($request);
 
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getEntityManager();
@@ -64,7 +74,6 @@ abstract class TableViewController extends AbstractTableViewController
         }
 
         $form = $this->createForm($this->_formType, $this->_entity);
-        $form->remove('row_order');
         $form->remove('check');
         
         if ($request->getMethod() == 'POST') {
@@ -107,6 +116,11 @@ abstract class TableViewController extends AbstractTableViewController
 
     protected function _swapRowOrder($id, $direction)
     {
+        /* Call the changeRowOrder method on this entity's repository
+         * Send the direction and Id.
+         * 
+         * @link \Corvus\AdminBundle\ILP\ORM\Repository\BaseEntityRepository
+         */
         $this->getDoctrine()->getEntityManager()
             ->getRepository('CorvusAdminBundle:' . $this->_entity->getRepoName())
             ->changeRowOrder($id, $direction);
@@ -123,8 +137,11 @@ abstract class TableViewController extends AbstractTableViewController
 
         $tableView = $request->request->get($this->_tableViewTypeName);
 
+        // TableView would ve submitted to the request if its a batch action
         if(isset($tableView)) {
+            // Attempt to delete every 'check'ed entity
             foreach ($tableView[$this->_tableViewDataName] as $entity) {
+                // The check field holds the ID of the entity
                 if(is_int((int)$entity['check']) == true)
                 {
                     $entityToRemove = $em
@@ -133,19 +150,24 @@ abstract class TableViewController extends AbstractTableViewController
 
                     if(isset($entityToRemove))
                     {
+                        // If the Entity is found using the id, remove the entity
                         $em->remove($entityToRemove);
                     }
                 }
             }
         }
+        // If just a single Id, the delete button next to an entity was clicked
         if(!$id == null)
         {
+            // Find the single entity by the Id
             $entityToRemove = $em->getRepository('CorvusAdminBundle:' . $this->_entity->getRepoName())
                 ->Find($id);
 
+            // Remove it
             $em->remove($entityToRemove);
         }
 
+        // Flush the Entity manager to save all deletions
         $em->flush();
 
         $this->get('session')->setFlash('notice', 'Selected ' . ucfirst($this->_entity->getName()) . ' was deleted!');
