d21e594478dadf20ded814776b20e0d6c163223e
Thomas Coleman <tom@ilikeprograms.com>
2014-05-14 15:57:26 +0100

    Stopped creating unused variables

--- src/Corvus/FrontendBundle/DependencyInjection/Configuration.php
+++ src/Corvus/FrontendBundle/DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('corvus_frontend');
+        $treeBuilder->root('corvus_frontend');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
