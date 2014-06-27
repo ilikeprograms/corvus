e4a0ed00b0688ceedf214913a531d03ee736db5f
Thomas Coleman <tom@ilikeprograms.com>
2014-06-27 17:22:28 +0100

    [Migrations] Added migration to Alter Education fields
    This was missing from the patch.

---
+++ app/DoctrineMigrations/Version20140618210907.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140618210907 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE Education ADD end_date DATE NOT NULL, ADD is_current_position TINYINT(1) NOT NULL, DROP duration");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE Education ADD duration NUMERIC(10, 0) NOT NULL, DROP end_date, DROP is_current_position");
+    }
+}
