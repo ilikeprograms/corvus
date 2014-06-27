5619e25c25a00dcd738fb8bd87fb6e60e3117e8b
Thomas Coleman <tom@ilikeprograms.com>
2014-06-27 17:29:17 +0100

    [Listener] Made ControllerListener check if template is set

--- src/Corvus/CoreBundle/EventListener/ControllerListener.php
+++ src/Corvus/CoreBundle/EventListener/ControllerListener.php
@@ -26,20 +26,21 @@ class ControllerListener
         // Get the Template details from the Template annotation
         $request = $event->getRequest();
         $template = $request->get('_template');
-
-        // Set the Controller to the Chosen Template
-        if ($template->get('bundle') === 'CorvusFrontendBundle') {
-            
-            $template->set('controller', $this->templateChoice);
-        }
         
-        if ($template->get('bundle') === 'CorvusAdminBundle') {
-            $template->set('controller', 'Default');
-        }        
+        if ($template) {
+            // Set the Controller to the Chosen Template
+            if ($template->get('bundle') === 'CorvusFrontendBundle') {
+                $template->set('controller', $this->templateChoice);
+            }
 
-        // Now render the chosen Template and set the Controller response to a response object with the data
-        $response = $this->templating->renderResponse($template, $controllerResult);
+            if ($template->get('bundle') === 'CorvusAdminBundle') {
+                $template->set('controller', 'Default');
+            }
 
-        $event->setResponse($response);
+            // Now render the chosen Template and set the Controller response to a response object with the data
+            $response = $this->templating->renderResponse($template, $controllerResult);
+
+            $event->setResponse($response);
+        }
     }
 }
